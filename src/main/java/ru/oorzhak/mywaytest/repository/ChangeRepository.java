46897ab5592822e184ff697f86a71e2ed393b1fe
daniiloorzhak <daniiloorzhak@gmail.com>
2023-07-18 12:32:20 +0700

    added methods for testing

--- src/main/java/ru/oorzhak/mywaytest/repository/ChangeRepository.java
+++ src/main/java/ru/oorzhak/mywaytest/repository/ChangeRepository.java
@@ -3,5 +3,9 @@ package ru.oorzhak.mywaytest.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.oorzhak.mywaytest.model.Change;
 
+import java.util.Optional;
+
 public interface ChangeRepository extends JpaRepository<Change, Long> {
+    Boolean existsByValue(Long value);
+    Optional<Change> findByValue(Long value);
 }
